6f079e0a5fd969ecae4b41402d1ec3f0f793f6ae
Mark McGinnis <mark.mcginnis@ufl.edu>
2021-10-25 23:56:57 -0400

    small scene changes for clarity

---
+++ FisheryManager/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void startGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("Quitting out of Game");
+        Application.Quit();
+    }
+
+    public void Guide()
+    {
+        Debug.Log("options pressed");
+    }
+}
